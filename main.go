59dbc6aeed29b46a5d9f8841edfa3031643e14cb
Махонины <makhoniny@MBAir-13-home.local>
2025-10-25 18:23:13 +0300

    1commit

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	//	"crypto/rand" если нужно включить не псевдогенерацию
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +16,101 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	if size <= 0 { //если запрошен невозможный размер слайса, возращаем пустой
+		return []int{}
+	}
+	slice := make([]int, size)
+	rnd := rand.NewSource(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		//		rnd, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt)) генерация случайных(не псевдо) чисел секунд 30 и вой кулеров... ну его...
+		slice[i] = int(rnd.Int63() + 1) //+1 для исключения нулевого значения- согласно условия "слайс целых положительных чисел"
+	}
+	return slice
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) <= 0 { //если слайс отрицательный или пустой- возвращаем 0
+		fmt.Println("Caution! the slice is empty!") //оповещаем юзера об особенностях данных
+		return 0
+	}
+	if len(data) == 1 { //если указан SIZE из одного значения- оно и будет максимальным
+		return data[0]
+	}
+
+	maxi := data[0]
+	for i := 1; i < len(data); i++ {
+		if data[i] > maxi {
+			maxi = data[i]
+		}
+	}
+	return maxi
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	// выполняем те же проверки, что и в maximum... запихнуть бы их в отдельную функцию
+	if len(data) <= 0 {
+		return 0
+	}
+	if len(data) == 1 {
+		return data[0]
+	}
+
+	slicik := make([]int, CHUNKS) // слайс с максимальными значениями кусков
+	piece := len(data) / CHUNKS   // размер куска, для обработки горутиной
+	var wg sync.WaitGroup
+	for i := 0; i < CHUNKS; i++ {
+		wg.Add(1)
+		go func() {
+			begin := i * piece //начальное значение куска
+			//считаем конечное значения куска, ибо оно может быть больше остальных
+			var end int
+			if i < CHUNKS-1 {
+				end = begin + piece
+			} else {
+				end = len(data) - 1
+			}
+			maks := data[end]
+			for rng := begin; rng < end; rng++ { //исключаем последний элемент, ибо его сравниваем изначально
+				if data[rng] > maks {
+					maks = data[rng]
+				}
+			}
+			slicik[i] = maks
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	maks := slicik[0]
+	for i := 1; i < len(slicik); i++ {
+		if slicik[i] > maks {
+			maks = slicik[i]
+		}
+	}
+	return maks
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	slice := generateRandomElements(SIZE)
 
-	fmt.Println("Ищем максимальное значение в один поток")
+	fmt.Print("Ищем максимальное значение в один поток ")
 	// ваш код здесь
+	start := time.Now()
+	maks := maximum(slice)
+	elapsed := time.Since(start)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d мкс\n", maks, elapsed.Microseconds()) //исправил ms на us, т.к. в задании вывод в микросекундах)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков ", CHUNKS)
 	// ваш код здесь
+	start = time.Now()
+	maks = maxChunks(slice)
+	elapsed = time.Since(start)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d мкс\n", maks, elapsed.Microseconds())
+
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,93 @@
 package main
 
+import (
+	"math"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// структура для тестов maximum и maxChunks
+type Tests struct {
+	note   string
+	slice  []int
+	answer int
+}
+
+var test = []Tests{
+	{
+		note:   "empty slice returns wrong answer",
+		slice:  []int{},
+		answer: 0,
+	},
+	{
+		note:   "one element maxInt in slice returns wrong answer",
+		slice:  []int{math.MaxInt},
+		answer: math.MaxInt,
+	},
+	{
+		note: "simple slice of 5 elements returned wrong answer",
+		slice: []int{
+			9, 6, math.MaxInt, 1, 21,
+		},
+		answer: math.MaxInt,
+	},
+	{
+		note: "wrong answer returns, when slice from identical values",
+		slice: []int{
+			1, 1, 1, 1,
+		},
+		answer: 1,
+	},
+}
+
 // Пишите тесты в этом файле
+// Тестируем функцию generateRandomElements
+func TestGenerateRandomElements(t *testing.T) {
+	slice := generateRandomElements(-5) //реакция программы на отрицательное значение
+	require.Equal(t, 0, len(slice), "get invalid sie of slice")
+	slice = generateRandomElements(0) //реакция программы на нулевое значение
+	require.Equal(t, 0, len(slice), "get invalid sie of slice")
+	slice = generateRandomElements(10) //реакция программы на положительное значение
+	assert.Equal(t, 10, len(slice), "get invalid sie of slice")
+	//проверим кусок слайса(10 элементов) на повторения значений
+	sort.Ints(slice) //отсортируем слайс
+	rep := 0         //счетчик повторяющихся значений
+	for i := 1; i < len(slice); i++ {
+		if slice[i-1] == slice[i] {
+			rep++
+		}
+		assert.Greater(t, 2, rep, "more then two repeats in random values") //если генератор повторит более 2 значений-предупредим
+	}
+	//так как по условию "слайс целых положительных чисел", проверим это условие
+	slice = generateRandomElements(1_000_000)
+	for i := 1; i < len(slice); i++ {
+		assert.Greater(t, slice[i], 1, "0 or negative number was found")
+	}
+}
+
+func TestMaximum(t *testing.T) {
+
+	for _, r := range test {
+		maxi := maximum(r.slice)
+		assert.Equal(t, r.answer, maxi, r.note)
+	}
+}
+
+func TestMaxChunks(t *testing.T) {
+	for _, r := range test {
+		maxi := maxChunks(r.slice)
+		assert.Equal(t, r.answer, maxi, r.note)
+	}
+	//	также очень интересно пробежать по всем элементам слайса, чтоб понимать, что 100% обрабатывается в горутинах
+	// ниже создается слайс заполненный 0, по очереди каждый элемент заполняется 9, проверяется, потом присваевается 1, процесс повторяется со следующим элементом
+	slice := make([]int, 10_000)
+	for i := 0; i < 10_000; i++ {
+		slice[i] = 9
+		maxi := maxChunks(slice)
+		assert.Equal(t, 9, maxi, "problem appeared during one-by-one test")
+		slice[i] = 1
+	}
+}
